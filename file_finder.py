f307ef951ef3d2f4bcc4887bb45bfa887245a7a2
MCMrARM <mcmrarm@gmail.com>
2019-01-01 15:11:36 +0100

    FileFinder: Implement patterns

--- file_finder.py
+++ file_finder.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 class FileFinderDir:
     def __init__(self, parent = None):
@@ -7,6 +8,16 @@ class FileFinderDir:
         self.include = -1
         self.exclude = -1
         self.wildcards = []
+
+class FileFinderWildcard:
+    def __init__(self, regex, include, index):
+        self.regex = re.compile(regex)
+        self.include = -1
+        self.exclude = -1
+        if include:
+            self.include = index
+        else:
+            self.exclude = index
 
 
 class FileFinder:
@@ -28,6 +39,26 @@ class FileFinder:
             d = d.subdirs[el]
         return d
 
+    def build_wildcard_regex(self, wildcard):
+        i = 0
+        regex = "^"
+        while True:
+            j = wildcard.find('*', i)
+            if j == -1:
+                break
+            regex += re.escape(wildcard[i:j])
+            if j + 1 < len(wildcard) and wildcard[j + 1] == '*':
+                # match anything
+                regex += '.*'
+                i = j + 2
+            else:
+                # match within the dir
+                regex += '[^\/]*'
+                i = j + 1
+        regex += re.escape(wildcard[i:])
+        regex += "($|\/)"
+        return regex
+
     def add_filter(self, what, include = True):
         first_wildcard = what.find("*")
         shared_part = what
@@ -39,8 +70,9 @@ class FileFinder:
         shared_part = os.path.realpath(shared_part)
         shared_part_dir = self.get_dir(shared_part)
         if wildcard_part is not None and len(wildcard_part) > 0:
-            raise Exception("Wildcards are not supported right now")
-        if include:
+            wildcard_regex = self.build_wildcard_regex(wildcard_part)
+            shared_part_dir.wildcards.append(FileFinderWildcard(wildcard_regex, include, self.next_rule_id))
+        elif include:
             shared_part_dir.include = self.next_rule_id
         else:
             shared_part_dir.exclude = self.next_rule_id
@@ -74,18 +106,26 @@ class FileFinder:
         for val in reversed(call_vals):
             dir_cb(val)
 
-    def _process(self, d, dstack, path, include_id, exclude_id, file_cb, dir_cb):
+    def _process(self, d, dstack, dpath, path, include_id, exclude_id, wildcards, file_cb, dir_cb):
         if not os.path.exists(path):
             return
         include_id = max(include_id, d.include)
         exclude_id = max(exclude_id, d.exclude)
+        wildcards = wildcards + d.wildcards
+        for w in wildcards:
+            if max(w.include, w.exclude) < max(include_id, exclude_id):
+                continue
+            if w.regex.match(path):
+                include_id = max(include_id, w.include)
+                exclude_id = max(exclude_id, w.exclude)
         if include_id > exclude_id:
             # include this dir
             self._process_parent_dirs(dstack, path, dir_cb)
             dir_cb(path)
             for e in os.scandir(path):
                 if e.is_dir() and e.name in d.subdirs:
-                    self._process(d.subdirs[e.name], dstack, e.path, include_id, exclude_id, file_cb, dir_cb)
+                    self._process(d.subdirs[e.name], dstack, os.path.join(dpath, e.name), e.path,
+                                  include_id, exclude_id, wildcards, file_cb, dir_cb)
                 elif e.is_dir():
                     dir_cb(e.path)
                     self._process_simple_dir(e.path, file_cb, dir_cb)
@@ -94,9 +134,10 @@ class FileFinder:
         else:
             dstack.append((d, False))
             for sname, sd in d.subdirs.items():
-                self._process(sd, dstack, os.path.join(path, sname), include_id, exclude_id, file_cb, dir_cb)
+                self._process(sd, dstack, os.path.join(dpath, sname), os.path.join(path, sname),
+                              include_id, exclude_id, wildcards, file_cb, dir_cb)
             dstack.pop()
 
     def process(self, root_path, file_cb, dir_cb):
         for name, d in self.dirs.items():
-            self._process(d, [], os.path.join(root_path, name), -1, -1, file_cb, dir_cb)
+            self._process(d, [], name, os.path.join(root_path, name), -1, -1, [], file_cb, dir_cb)
